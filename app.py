36198f4e16abf81ff61dd8f9f9356c87f94efee6
Finot2023 <yagotokko@gmail.com>
2026-06-15 13:48:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 import os
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__, template_folder='app_templates')
 app.secret_key = "finot_super_secret_key"
 
